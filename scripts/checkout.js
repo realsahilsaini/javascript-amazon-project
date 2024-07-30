8a4b9ef84fe63d7a80501b4ba087198cae164592
realsahilsaini <thisissahilsaini@gmail.com>
2024-07-31 00:10:21 +0530

    made separate function in money.js for price formatting and fixed radio button by giving unique name

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import {cart} from "../data/cart.js";
 import { products } from "../data/products.js";
+import  {formatCurrency} from "./utils/money.js";
 
 let cartSummary = '';
 
@@ -31,7 +32,7 @@ cart.forEach((cartItem) => {
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  ${matchingProduct.priceCents / 100}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -53,7 +54,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio" checked
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Tuesday, June 21
@@ -66,7 +67,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Wednesday, June 15
@@ -79,7 +80,7 @@ cart.forEach((cartItem) => {
                 <div class="delivery-option">
                   <input type="radio"
                     class="delivery-option-input"
-                    name="delivery-option-1">
+                    name="delivery-option-${matchingProduct.id}">
                   <div>
                     <div class="delivery-option-date">
                       Monday, June 13
@@ -97,4 +98,11 @@ cart.forEach((cartItem) => {
 
 
 document.querySelector('.js-cart-summary').innerHTML = cartSummary;
+
+
+/*
+Notes: 
+- To avoid the Selection of radio button in the delivery options, we can use the name attribute to group the radio buttons together. This way, only one radio button can be selected at a time.
+*/
+
 
