cb60eb7ef3d94391c17353702b1715df410131cc
realsahilsaini <thisissahilsaini@gmail.com>
2024-07-31 00:47:13 +0530

    Js logic when delete products from cart

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,12 @@
-export const cart =[{
+export let cart =[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
 },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+
+// ---------------------------------------------------
 
 export function addToCart(productId) {
   let matchingItem;
@@ -23,4 +25,26 @@ export function addToCart(productId) {
       quantity: 1,
     });
   }
+}
+
+// ---------------------------------------------------
+
+export function removeFromCart(productId) {
+
+  const newCart = [];
+  cart.forEach((cartItem) => {
+    if (productId !== cartItem.productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
+
+//another logic using splice method
+  // let matchingItemIndex;
+  // cart.forEach((cartItem, index) => {
+  //   if (productId === cartItem.productId) {
+  //     matchingItemIndex = index;
+  //   }
+  // });
+  // cart.splice(matchingItemIndex, 1);
 }
