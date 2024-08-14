cbe852b543af5ed704a6b135862bdcc390a57c9b
realsahilsaini <thisissahilsaini@gmail.com>
2024-08-14 23:17:20 +0530

    Load products in beforeAll hook for jasmine's orderSummary test

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,5 +1,6 @@
  import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
  import {loadFromStorage, cart} from '../../data/cart.js';
+ import { loadProducts } from "../../data/products.js";
 
 
  describe("renderOrderSummary", () => {
@@ -7,6 +8,12 @@
   const productId1 = 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6'
   const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d'
 
+  //hook
+  beforeAll((done) => {
+    loadProducts(() => {
+      done(); //This is a callback function that tells Jasmine that the async operation is done and it can proceed with the tests.
+    });
+  });
 
   //hook
   beforeEach(() => {
